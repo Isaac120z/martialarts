7a5938874616a79c7089aa506ead312082511ed8
Isaac <Isaac120z@msn.com>
2018-08-14 17:24:40 -0500

    added names to show in UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,37 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnChanges, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  title = '';
+  title = "";
   names = [];
-  constructor(private httpClient: HttpClient){
-
-  }
+  constructor(private httpClient: HttpClient) {}
   ngOnInit(): void {
-    this.httpClient.get('./assets/martialarts.json').subscribe(response => {
-      console.log('Response', response);
-      this.title = response['title'];
-      console.log('martialArts', response['martialArts']);
+    this.httpClient.get("./assets/martialarts.json").subscribe(response => {
+      console.log("Response", response);
+      this.title = response["title"];
+      console.log("martialArts", response["martialArts"]);
       let forEachNames = [];
-      response['martialArts'].forEach(martialart => {
+      response["martialArts"].forEach(martialart => {
         forEachNames.push(martialart.name);
       });
-      console.log('forEachNames', forEachNames);
+      console.log("forEachNames", forEachNames);
 
-      let mapNames = response['martialArts'].map( martialart => {
+      let mapNames = response["martialArts"].map(martialart => {
         return martialart.name;
       });
-      console.log('mapNames', mapNames);
+      console.log("mapNames", mapNames);
+      this.names = mapNames;
     });
-    
-    this.httpClient.get('https://jsonplaceholder.typicode.com/todos/1').subscribe(data => {
-      console.log('data', data);
-    });
-  }
 
+    this.httpClient
+      .get("https://jsonplaceholder.typicode.com/todos/1")
+      .subscribe(data => {
+        console.log("data", data);
+      });
+  }
 }
