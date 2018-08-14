92fc8db49ed639446e10faf9f0b39933ba83c3ce
huantbui <huantbui@gmail.com>
2018-08-14 14:26:04 -0500

    reference title for app component html to render with the json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,27 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app testset!';
-  
+  title = '';
+  names = [];
   constructor(private httpClient: HttpClient){
 
   }
   ngOnInit(): void {
-    // this.httpClient.get('./assets/martialarts.json').subscribe(response => {
-    //   console.log('Response', response);
-    //   console.log('martialArts', response['martialArts']);
-    //   let forEachNames = [];
-    //   response['martialArts'].forEach(martialart => {
-    //     forEachNames.push(martialart.name);
-    //   });
-    //   console.log('forEachNames', forEachNames);
+    this.httpClient.get('./assets/martialarts.json').subscribe(response => {
+      console.log('Response', response);
+      this.title = response['title'];
+      console.log('martialArts', response['martialArts']);
+      let forEachNames = [];
+      response['martialArts'].forEach(martialart => {
+        forEachNames.push(martialart.name);
+      });
+      console.log('forEachNames', forEachNames);
 
-    //   let mapNames = response['martialArts'].map( martialart => {
-    //     return martialart.name;
-    //   });
-    //   console.log('mapNames', mapNames);
-    // });
+      let mapNames = response['martialArts'].map( martialart => {
+        return martialart.name;
+      });
+      console.log('mapNames', mapNames);
+    });
     
     this.httpClient.get('https://jsonplaceholder.typicode.com/todos/1').subscribe(data => {
       console.log('data', data);
